93b70b03ac584654882d8770de68b80682d4d7a4
alireza valizade <alirezavalizade0@gmail.com>
2017-08-20 15:53:05 +0430

    sass added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <Carousel />
+        <Carousel>
+          <h1>1</h1>
+          <h1>2</h1>
+          <h1>3</h1>
+          <h1>4</h1>
+        </Carousel>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
