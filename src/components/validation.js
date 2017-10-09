1dcffa759d27870d807da35f73597e575abed855
alireza valizade <alirezavalizade0@gmail.com>
2017-10-09 14:12:02 +0330

    support function for single items and pass value from function

--- src/components/validation.js
+++ src/components/validation.js
@@ -2,7 +2,12 @@ import { createReduxFormValidator, validations } from 'redux-form-validation-wit
 
 const validate = createReduxFormValidator({
   firstname: [validations.required, validations.alphabet],
-  lastname: [validations.required, validations.alphabet],
+  lastname: (v) => {
+    if (v > 0) {
+      return [validations.required, validations.alphabet];
+    }
+    return [];
+  },
   people: {
     name: [validations.required, validations.alphabet],
     value: [validations.required]
